810e4c3149256e3d63762d24865edf33391e0a91
Raul <Raul@Raul-PC>
2017-05-26 11:42:03 -0500

    algo

--- src/pw2/PW2_IEServlet.java
+++ src/pw2/PW2_IEServlet.java
@@ -8,5 +8,8 @@ public class PW2_IEServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, world");
 	}
 }
